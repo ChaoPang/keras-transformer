dc480a257fc092d1e0808e3ad8e9706c3cf8b1e9
Chao Pang <ChaoPang229@gmail.com>
2020-06-15 21:16:55 -0400

    added time_attention_model for concept embeddings

--- bert_concept_embeddings/model.py
+++ bert_concept_embeddings/model.py
@@ -3,21 +3,72 @@ import tensorflow as tf
 # +
 import tensorflow as tf
 
-#from keras import regularizers
-#from keras.models import Model
+# from keras import regularizers
+# from keras.models import Model
 # noinspection PyPep8Naming
 
 from keras_transformer.extras import ReusableEmbedding, TiedOutputEmbedding
 from keras_transformer.position import TransformerCoordinateEmbedding
 from keras_transformer.transformer import TransformerACT, TransformerBlock
 
-from bert_concept_embeddings.custom_layers import EncoderLayer, TimeAttention
+from bert_concept_embeddings.custom_layers import EncoderLayer, TimeSelfAttention, TimeAttention
 
 
 # -
 
-# -
+def time_attention_model(max_seq_length: int,
+                         vocabulary_size: int,
+                         concept_embedding_size: int):
+    """
 
+    :param max_seq_length:
+    :param vocabulary_size:
+    :param concept_embedding_size:
+    :return:
+    """
+    target_concepts = tf.keras.layers.Input(shape=(1,), dtype='int32', name='target_concept_id')
+
+    target_time_stamps = tf.keras.layers.Input(shape=(1,), dtype='int32', name='target_time_stamps')
+
+    context_concepts = tf.keras.layers.Input(shape=(max_seq_length,), dtype='int32', name='context_concept_ids')
+
+    context_time_stamps = tf.keras.layers.Input(shape=(max_seq_length,), dtype='int32', name='context_time_stamps')
+
+    mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype='int32', name='mask')
+
+    embedding_layer = tf.keras.layers.Embedding(vocabulary_size, concept_embedding_size, name='embedding_layer',
+                                                mask_zero=True)
+
+    time_embedding_layer = TimeAttention(vocab_size=vocabulary_size, target_seq_len=1, context_seq_len=max_seq_length)
+
+    dense_layer = tf.keras.layers.Dense(vocabulary_size)
+
+    softmax_layer = tf.keras.layers.Softmax()
+
+    # shape = (batch_size, seq_len, embedding_size)
+    concept_embeddings = embedding_layer(context_concepts)
+
+    # shape = (batch_size, 1, seq_len)
+    time_embeddings = time_embedding_layer(target_concepts=target_concepts,
+                                           context_concepts=context_concepts,
+                                           target_time_stamps=target_time_stamps,
+                                           context_time_stamps=context_time_stamps,
+                                           time_mask=mask)
+
+    # shape = (batch_size, 1, embedding_size)
+    combined_embeddings = tf.matmul(time_embeddings, concept_embeddings)
+
+    # shape = (batch_size, 1, vocab_size)
+    concept_predictions = softmax_layer(dense_layer(combined_embeddings))
+
+    model = tf.keras.Model(
+        inputs=[target_concepts, target_time_stamps, context_concepts, context_time_stamps, mask],
+        outputs=[concept_predictions])
+
+    return model
+
+
+# -
 def transformer_bert_model(
         max_seq_length: int,
         vocabulary_size: int,
@@ -44,9 +95,7 @@ def transformer_bert_model(
 
     mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype='int32', name='mask')
 
-    time_mask = tf.expand_dims(mask, axis=1)
-
-    concept_mask = tf.expand_dims(time_mask, axis=1)
+    concept_mask = tf.expand_dims(tf.expand_dims(mask, axis=1), axis=1)
 
     l2_regularizer = (tf.keras.regularizers.l2(l2_reg_penalty) if l2_reg_penalty else None)
 
@@ -59,7 +108,7 @@ def transformer_bert_model(
         # https://arxiv.org/pdf/1508.03721.pdf
         embeddings_regularizer=l2_regularizer)
 
-    time_embedding_layer = TimeAttention(vocab_size=vocabulary_size, seq_len=max_seq_length)
+    time_embedding_layer = TimeSelfAttention(vocab_size=vocabulary_size, seq_len=max_seq_length)
 
     output_layer = TiedOutputEmbedding(
         projection_regularizer=l2_regularizer,
@@ -76,7 +125,9 @@ def transformer_bert_model(
     # "Attention is all you need", 2017)
     next_step_input = coordinate_embedding_layer(next_step_input, step=0)
 
-    time_attention_logits = time_embedding_layer(concept_ids, time_stamps, time_mask)
+    time_attention_logits = time_embedding_layer(concept_ids, time_stamps, mask)
+    # pad a dimension to accommodate the head split
+    time_attention_logits = tf.expand_dims(time_attention_logits, axis=1)
 
     for i in range(d_model):
         next_step_input = (
